a8760389db1fc1cc04f50f97c8accd4479cf8fd3
Ciro <cironidas@gmail.com>
2024-01-24 10:49:36 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,57 +1,32 @@
 import streamlit as st
-import os
+
+st.title("Simulador de neurona")
+
+# Slider para seleccionar el número de entradas/pesos
+num_entradas = st.slider("Elige el número de entradas/pesos que tendrá la neurona", min_value=1, max_value=10, value=3, key="s_pe")
+
+# Crear columnas para los inputs de pesos
+st.write("#### Pesos")
+pesos = []
+for i in range(num_entradas):
+    peso = st.number_input(f'w{i}', key=f'w{i}')
+    pesos.append(peso)
 
 
+st.write(f'Pesos: {pesos}')
 
-ruta_imagen_local = os.path.join("img", "2480958_15286-removebg-preview.png")
-st.image(ruta_imagen_local)
+# Crear columnas para los inputs de entradas
+st.write("#### Entradas")
+entradas = []
+for i in range(num_entradas):
+    entrada = st.number_input(f'x{i}', key=f'x{i}')
+    entradas.append(entrada)
 
-st.title("¡Hola neurona!")
+# Botón para calcular salida
+btn_salida = st.button("Calcular salida", key="btn_salida")
 
+if btn_salida:
+    # Calcular la salida de la neurona
+    y = sum(p * x for p, x in zip(pesos, entradas))
+    st.write("La salida de la neurona es", y)
 
-tab1, tab2, tab3 = st.tabs(["Una entrada", "Dos entradas", "Tres entradas"])
-
-with tab3:
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        w0 = st.slider("Peso w₀", min_value=0.0, max_value=5.0, value=2.5, key="w0_t3")
-        x0 = st.number_input("Entrada x₀", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x0_t3")
-    with col2:
-        w1 = st.slider("Peso w₁", min_value=0.0, max_value=5.0, value=2.5, key="w1_t3")
-        x1 = st.number_input("Entrada x₁", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x1_t3")
-    with col3:
-        w2 = st.slider("Peso w₂", min_value=0.0, max_value=5.0, value=2.5, key="w2_t3")
-        x2 = st.number_input("Entrada x₂", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x2_t3")
-    b = st.number_input("Introduzca el sesgo", key="b_t3")
-    boton_calcular_salida_t3 = st.button("Calcular salida", key="boton_t3")
-
-    if boton_calcular_salida_t3:
-        y = x0 * w0 + x1 * w1 + x2 * w2 + b
-        st.write("Salida:", y)
-
-with tab2:
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        w0 = st.slider("Peso w₀", min_value=0.0, max_value=5.0, value=2.5, key="w0_t2")
-        x0 = st.number_input("Entrada x₀", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x0_t2")
-    with col2:
-        w1 = st.slider("Peso w₁", min_value=0.0, max_value=5.0, value=2.5, key="w1_t2")
-        x1 = st.number_input("Entrada x₁", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x1_t2")
-
-    boton_calcular_salida_t2 = st.button("Calcular salida", key="boton_t2")
-
-    if boton_calcular_salida_t2:
-        y = x0 * w0 + x1 * w1
-        st.write("Salida:", y)
-
-with tab1:
-    w = st.slider("Peso w", min_value=0.0, max_value=5.0, value=2.5, key="w_t1")
-    x = st.number_input("Entrada x", min_value=2.0, max_value=3.0, value=2.5, format="%f", key="x_t1")
-    boton_calcular_salida_t1 = st.button("Calcular salida", key="boton_t1")
-
-    if boton_calcular_salida_t1:
-        y = x * w
-        st.write("Salida:", y)
