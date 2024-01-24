83e49909e2fc605e6154beae542ffa2bcc85f43a
Ciro <cironidas@gmail.com>
2024-01-24 11:21:19 +0100

    2

--- src/app.py
+++ src/app.py
@@ -1,33 +1,45 @@
 import streamlit as st
+from neurona import Neuron
 
 st.title("Simulador de neurona")
 
-
 num_x_w = st.slider("Elige el número de entradas/pesos que tendrá la neurona", min_value=1, max_value=10, value=3, key="s_pe")
 
-# Crear columnas para los inputs de pesos
-st.write("## Pesos")
-pesos = []
-for col in st.columns(num_x_w):
-    with col:
-        peso = st.number_input(f'w{len(pesos)}', key=f'w{len(pesos)}')
-        pesos.append(peso)
+# Valores por defecto para el caso n1 = Neuron(weights=[0.5, 1.2, 6.8], bias=100, func="relu")
+default_pesos = [0.5, 1.2, 6.8]
+default_entradas = [4, 6, -3]
+default_sesgo = 100
+default_func = "relu"
 
+# Widgets para que el usuario pueda modificar los valores por defecto
+pesos = []
+for col, default_peso in zip(st.columns(num_x_w), default_pesos):
+    with col:
+        peso = st.number_input(f'w<sub>{len(pesos)}</sub>', key=f'w{len(pesos)}', value=default_peso)
+        pesos.append(peso)
 
 st.write(f'w = {pesos}')
 
-# Crear columnas para los inputs de entradas
-st.write("## Entradas")
 entradas = []
-for i in range(num_x_w):
-    entrada = st.number_input(f'x{i}', key=f'x{i}')
-    entradas.append(entrada)
+for col, default_entrada in zip(st.columns(num_x_w), default_entradas):
+    with col:
+        entrada = st.number_input(f'x<sub>{len(entradas)}</sub>', key=f'x{len(entradas)}', value=default_entrada)
+        entradas.append(entrada)
 
-# Botón para calcular salida
+st.write(f'x = {entradas}')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.write("### Sesgo")
+    sesgo = st.number_input("Introduce el valor del sesgo", value=default_sesgo)
+with col2:
+    st.write("### Función de activación")
+    func_options = ["relu", "sigmoid", "tanh"]
+    func = st.selectbox("Elige la función de activación", func_options, index=func_options.index(default_func))
+
 btn_salida = st.button("Calcular salida", key="btn_salida")
 
 if btn_salida:
-    # Calcular la salida de la neurona
-    y = sum(p * x for p, x in zip(pesos, entradas))
+    neurona = Neuron(weights=pesos, bias=sesgo, func=func)
+    y = neurona.run(entradas)
     st.write("La salida de la neurona es", y)
-
