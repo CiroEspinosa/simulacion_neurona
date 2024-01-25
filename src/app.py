fda36fab154345087fddd66155f23d87c6402abd
Ciro <cironidas@gmail.com>
2024-01-25 09:09:24 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,38 +2,36 @@ import streamlit as st
 from neurona import Neuron
 import os
 
+st.set_page_config(layout="wide")
 ruta_imagen_local = os.path.join("img", "2480958_15286-removebg-preview.png")
-st.image(ruta_imagen_local)
+st.image(ruta_imagen_local, width=200)
 st.title("Simulador de neurona")
 
 num_x_w = st.slider("Elige el número de entradas/pesos que tendrá la neurona", min_value=1, max_value=10, value=3, key="s_pe")
 
-# Valores por defecto para el caso n1 = Neuron(weights=[0.5, 1.2, 6.8], bias=100, func="relu")
-default_pesos = [0.5, 1.2, 6.8]
-default_entradas = [4, 6, -3]
-default_sesgo = 100
-default_func = "relu"
+def_pesos = [0.5, 1.2, 6.8]
+def_entradas = [4, 6, -3]
+def_sesgo = 100
+def_func = "relu"
 
-# Asegurarse de que hay suficientes valores por defecto para las columnas
-default_pesos += [0] * max(0, num_x_w - len(default_pesos))
-default_entradas += [0] * max(0, num_x_w - len(default_entradas))
+def_pesos += [0] * max(0, num_x_w - len(def_pesos))
+def_entradas += [0] * max(0, num_x_w - len(def_entradas))
 
-# Widgets para que el usuario pueda modificar los valores por defecto
 
 pesos = []
 st.write("### Pesos")
-for col, default_peso in zip(st.columns(num_x_w), default_pesos):
+for col, def_peso in zip(st.columns(num_x_w), def_pesos):
     with col:
-        peso = st.number_input(f'w$_{len(pesos)}$', key=f'w{len(pesos)}', value=float(default_peso))
+        peso = st.number_input(f'w$_{len(pesos)}$', key=f'w{len(pesos)}', value=float(def_peso))
         pesos.append(peso)
 
 st.write(f'w = {pesos}')
 
 entradas = []
 st.write("### Entradas")
-for col, default_entrada in zip(st.columns(num_x_w), default_entradas):
+for col, def_entrada in zip(st.columns(num_x_w), def_entradas):
     with col:
-        entrada = st.number_input(f'x$_{len(entradas)}$', key=f'x{len(entradas)}', value=float(default_entrada))
+        entrada = st.number_input(f'x$_{len(entradas)}$', key=f'x{len(entradas)}', value=float(def_entrada))
         entradas.append(entrada)
 
 st.write(f'x = {entradas}')
@@ -41,11 +39,11 @@ st.write(f'x = {entradas}')
 col1, col2 = st.columns(2)
 with col1:
     st.write("### Sesgo")
-    sesgo = st.number_input("Introduce el valor del sesgo", value=float(default_sesgo))
+    sesgo = st.number_input("Introduce el valor del sesgo", value=float(def_sesgo))
 with col2:
     st.write("### Función de activación")
     func_options = ["relu", "sigmoid", "tanh"]
-    func = st.selectbox("Elige la función de activación", func_options, index=func_options.index(default_func))
+    func = st.selectbox("Elige la función de activación", func_options, index=func_options.index(def_func))
 
 btn_salida = st.button("Calcular salida", key="btn_salida")
 
